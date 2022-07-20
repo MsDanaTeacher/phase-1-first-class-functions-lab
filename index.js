e5bc97730a6b2ffcfd8b5be0987a7cd7c7a6d22a
Dana Lobell <danalobell@gmail.com>
2022-07-20 15:58:32 -0400

    commit tree 9221afdf635c5eb598e3fcb48172714471f30681

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = array => {
+    return array.slice(0,2);
+}
+
+const returnLastTwoDrivers = array => {
+    return array.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (integer) {
+    return function price(fare) {
+        return fare * integer;
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(array, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(array);
+}
